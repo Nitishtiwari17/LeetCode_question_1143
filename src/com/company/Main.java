9294d7b3df6128bc1477e2051093c166fb2a3ae8
Nitish Kumar Tiwari <nitishtiwari1710@gmail.com>
2022-09-21 01:12:42 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+    public int longestCommonSubsequence(String text1, String text2) {
+        char[] str1=text1.toCharArray();
+        char[] str2=text2.toCharArray();
+        int temp[][]=new int[str1.length+1][str2.length+1];
+        int max=0;
+        for(int i=0;i<temp.length;i++){
+            for(int j=0;j<temp[i].length;j++){
+                if(str1[i-1]==str2[j-1]){
+                    temp[i][j]=temp[i-1][j-1]+1;
+                }
+                else{
+                    temp[i][j]=Math.max(temp[i][j-1],temp[i-1][j]);
+                }
+                if(temp[i][j]>max){
+                    max=temp[i][j];
+                }
+            }
+        }
+        return max;
+    }
+}
+
